0c342a8983e8e77ac802b3a7347600a68c929420
Bhautik Makvana <bhautikmakvana1310@gmail.com>
2024-08-10 18:29:12 +0000

    DB connection part 2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import mongoose from "mongoose";
-import {DB_NAME} from "./constance";
-import express from "express";
+// require('dotenv').config({path: './env.sample'})  // method 1
 
-const app = express();
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.sample.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.log("error : ", error)
-            throw error
-        })
-        app.listen(process.env.sample.PORT, () => {
-            console.log(`app is listening on ${process.env.sample.PORT}`);
-        })
-    } catch (error) {
-        console.log("Error", error);
-        throw error
-    }
-})()
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constance";
+// import express from "express";
+import dotenv from "dotenv"; //    method 2
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env.sample'     //    with method 2
+})
+
+connectDB()
+
+// const app = express();
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process.env.sample.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("error : ", error)
+//             throw error
+//         })
+//         app.listen(process.env.sample.PORT, () => {
+//             console.log(`app is listening on ${process.env.sample.PORT}`);
+//         })
+//     } catch (error) {
+//         console.log("Error", error);
+//         throw err
+//     }
+// })()
